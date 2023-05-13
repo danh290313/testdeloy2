8f0fa6512c1f712a6bc03bc285064d526172f12f
danh <danh290313@gmail.com>
2023-05-13 23:18:33 +0700

    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 import javax.websocket.server.PathParam;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,7 +50,27 @@ public class DemoApplication {
 			return ResponseEntity.notFound().build();
 		return ResponseEntity.ok(user.get());
 	}
+	
+	@GetMapping("users")
+	public ResponseEntity<Object> getUsers() {
 
+		List<User> users = userRepository.findAll();
+		if (users.isEmpty())
+			return ResponseEntity.notFound().build();
+
+		return ResponseEntity.ok(users);
+	}
+	
+	@DeleteMapping("user/{id}")
+	public ResponseEntity<Object> deleteUser(Long id) {
+		Optional<User> user = userRepository.findById(id);
+		if (user.isEmpty())
+			return ResponseEntity.notFound().build();
+		
+		userRepository.deleteById(id);
+		return ResponseEntity.ok("Delete successfully");	
+	
+	}
 
 
 }
