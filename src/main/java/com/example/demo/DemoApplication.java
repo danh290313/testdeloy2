c483ad5fdadad6d7883d82d78bb204c6f518a256
danh <danh290313@gmail.com>
2023-05-13 22:32:51 +0700

    add

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,17 @@ package com.example.demo;
 
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
+
+import java.util.Optional;
+
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -26,12 +33,22 @@ public class DemoApplication {
 	}
 
 	@GetMapping("add-user")
-	public String addUser(){
+	public String addUser() {
 		User user = new User();
 		user.setFirstName("Render");
 		user.setLastName("Spring Boot");
 		userRepository.save(user);
 		return "User added successfully";
 	}
+	
+	@GetMapping("user/{id}")
+	public ResponseEntity<Object> add(@PathVariable Long id) {
+		Optional<User> user = userRepository.findById(id);
+		if(user.isEmpty())
+			return ResponseEntity.notFound().build();
+		return ResponseEntity.ok(user.get());
+	}
+
+
 
 }
